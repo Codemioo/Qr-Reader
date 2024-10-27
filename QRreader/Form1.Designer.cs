7fc826f0d9d1405c4589b06c030732e4a96cad3e
rabia kılaçak <150595756+Codemioo@users.noreply.github.com>
2024-10-27 21:45:46 +0300

    QR Reader
    C# . NET Framework üzerinden basit bir QR kod okuyucu programı.

---
+++ QRreader/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace QRreader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startbtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.webControl1 = new EO.WinForm.WebControl();
+            this.webView1 = new EO.WebBrowser.WebView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Camera:";
+            // 
+            // startbtn
+            // 
+            this.startbtn.Location = new System.Drawing.Point(590, 59);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(79, 31);
+            this.startbtn.TabIndex = 2;
+            this.startbtn.Text = "Start";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(118, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 241);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(412, 352);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(312, 61);
+            this.textBox2.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(200, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(373, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 61);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Reader";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(535, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 31);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "QR Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // webControl1
+            // 
+            this.webControl1.BackColor = System.Drawing.Color.White;
+            this.webControl1.Location = new System.Drawing.Point(412, 107);
+            this.webControl1.Name = "webControl1";
+            this.webControl1.Size = new System.Drawing.Size(312, 241);
+            this.webControl1.TabIndex = 9;
+            this.webControl1.Text = "webControl1";
+            this.webControl1.WebView = this.webView1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webControl1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private EO.WinForm.WebControl webControl1;
+        private EO.WebBrowser.WebView webView1;
+    }
+}
+
---
+++ QRreader/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing.Aztec;
+using EO.WebBrowser;
+
+
+namespace QRreader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private FilterInfoCollection CaptureDevice;
+       private  VideoCaptureDevice FinalFrame;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in CaptureDevice)
+            {
+                comboBox1.Items.Add(Device.Name);
+
+            }
+            FinalFrame = new VideoCaptureDevice();
+           
+        }
+
+       
+        private void startbtn_Click(object sender, EventArgs e)
+        {
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[comboBox1.SelectedIndex].MonikerString);
+            FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+            FinalFrame.Start();
+
+        }
+        private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+           // pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+
+            // Mevcut çerçeveden QR kodunu çözün
+            BarcodeReader reader = new BarcodeReader();
+            Result result = reader.Decode((Bitmap)eventArgs.Frame.Clone());
+            if (result != null)
+            {
+                if (Uri.IsWellFormedUriString(result.Text, UriKind.Absolute))
+                {
+                    // Open URL in browser/Eo.WebBrowser (replace with desired method)
+                    webView1.LoadUrl(result.Text);
+                }
+                else
+                {
+                    textBox2.Text = "Geçersiz URL";
+                }
+            }
+
+
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (FinalFrame.IsRunning==true)
+            {
+                FinalFrame.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*
+            BarcodeReader reader = new BarcodeReader();
+            Result result = reader.Decode((Bitmap)pictureBox1.Image);
+            try
+            {
+                if (result.ToString().Trim() == "")
+                {
+                    return;
+                }
+                textBox2.Text = result.ToString().Trim();
+            }
+            catch (Exception ex)
+            { 
+            }*/
+            try
+            {
+                BarcodeReader reader = new BarcodeReader();
+                Result result = reader.Decode((Bitmap)pictureBox1.Image);
+                // textBox2.Text = result.Text;
+
+                if (result != null)
+                {
+                    /*
+                    if (Uri.IsWellFormedUriString(result.Text, UriKind.Absolute))
+                    {
+                        // URL'yi tarayıcıda açın
+                        webBrowser1.Navigate(result.Text);
+                    }*/
+                    if (Uri.IsWellFormedUriString(result.Text, UriKind.Absolute))
+                    {
+                        // URL'yi Eo.WebBrowser'da açın
+                        webView1.LoadUrl(result.Text);
+                    }
+                    else
+                    {
+                        // Geçersiz URL mesajı göster
+                        textBox2.Text = "Geçersiz URL";
+                    }
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                // Geçersiz görüntü hatası
+                textBox2.Text = "Hata: Geçersiz Görüntü";
+                Console.WriteLine("Hata (ArgumentNullException): " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // Genel hata yakalama
+                textBox2.Text = "Hata: QR kodunu çözümlemede hata oluştu";
+                Console.WriteLine("Genel Hata: " + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Title = "Resim Dosyaları";
+            saveFile.Filter = "PNG dosyası|*.png";
+            if (saveFile.ShowDialog()== DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFile.FileName);
+                MessageBox.Show("kayıt işlemi başarılı");
+            }
+        }
+    }
+}
